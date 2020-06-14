f0c104d8dd63f4bd28a195f6f7e1ccfd1f4aefa2
Louis Popi <theh2o64@gmail.com>
2020-06-14 09:15:39 +0200

    script_java_format: Support new NLP parameters

--- script_java_format.sh
+++ script_java_format.sh
@@ -5,6 +5,6 @@ java_lib=$4
 count=0
 
 for file in $(find $in_folder -type f); do
-  java -cp $java_lib org.lara.nlp.context.FacebookTest $file "$answerer" "$out_folder/conv_$count.txt"
+  java -cp $java_lib org.lara.nlp.context.FacebookTest -fb_json $file -answerer "$answerer" -export "$out_folder/conv_$count.txt" "$5"
   count=$(expr $count + 1)
 done
