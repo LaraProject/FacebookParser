9975f2bb11195d6d5372e9c84d9ef94c47726821
Louis Popi <theh2o64@gmail.com>
2020-06-14 14:53:34 +0200

    script_java_format: Force min_length and max_length
    * I trust no one

--- script_java_format.sh
+++ script_java_format.sh
@@ -5,6 +5,6 @@ java_lib=$4
 count=0
 
 for file in $(find $in_folder -type f); do
-  java -cp $java_lib org.lara.nlp.context.FacebookTest -fb_json $file -answerer "$answerer" -export "$out_folder/conv_$count.txt" "$5"
+  java -cp $java_lib org.lara.nlp.context.FacebookTest -fb_json $file -answerer "$answerer" -export "$out_folder/conv_$count.txt" -min_length 0 -max_length 40
   count=$(expr $count + 1)
 done
